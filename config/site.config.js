4d6323840ebaaf2c9ec6a3c488f9fc8d0e55639d
tkfanswind <55653816+tkfanswind@users.noreply.github.com>
2022-02-11 19:57:57 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -35,7 +35,7 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    'Powered by <a href="https://github.com/spencerwooo/onedrive-vercel-index" target="_blank" rel="noopener noreferrer">onedrive-vercel-index</a>. Made with ❤ by SpencerWoo.',
+    '<a href="https://komuro.cf"><img src="https://img.shields.io/badge/Tetsuya%20Komuro%20One%20Drive-000000?logo=Apple&logoColor=FFFFFF" alt="tkfans"></a>',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
